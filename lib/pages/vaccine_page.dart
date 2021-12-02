5e7f960eb6ed548a6abe5d76c980757eab144015
Suhael Farouk <suhaelfarouk@Suhaels-MacBook-Pro.local>
2021-12-02 23:46:35 +0530

    commit

--- lib/pages/vaccine_page.dart
+++ lib/pages/vaccine_page.dart
@@ -42,165 +42,195 @@ class _VaccinePageState extends State<VaccinePage> {
           ? Colors.black45
           : Colors.black12,
       //appBar: AppBar(title: Text('Vaccination Slots')),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.all(10),
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: Column(children: [
-            Container(
-              margin: EdgeInsets.all(20),
-              height: 250,
-              // child: Image.asset('assets/vaccine.png'),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        // height: MediaQuery.of(context).size.height,
+        // width: MediaQuery.of(context).size.width,
+        child: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+          // Container(
+          //   margin: EdgeInsets.all(20),
+          //   height: 200,
+          //   // child: Image.asset('assets/vaccine.png'),
+          // ),
+          TextField(
+            controller: districtCodecontroller,
+            keyboardType: TextInputType.number,
+            maxLength: 6,
+            decoration: InputDecoration(
+              hintText: 'Enter District Code',
+              fillColor: Theme.of(context).brightness == Brightness.dark
+                  ? Colors.black
+                  : Colors.white,
+              filled: true,
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide.none),
             ),
-            TextField(
-              controller: districtCodecontroller,
-              keyboardType: TextInputType.number,
-              maxLength: 6,
-              decoration: InputDecoration(
-                hintText: 'Enter District Code',
-                fillColor: Theme.of(context).brightness == Brightness.dark
-                    ? Colors.black
-                    : Colors.white,
-                filled: true,
-                border: OutlineInputBorder(
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).brightness == Brightness.dark
+                        ? Colors.black
+                        : Colors.white,
                     borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide.none),
-              ),
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).brightness == Brightness.dark
-                          ? Colors.black
-                          : Colors.white,
-                      borderRadius: BorderRadius.circular(10),
+                  ),
+                  height: 57,
+                  child: DropdownButton<String>(
+                    enableFeedback: true,
+                    elevation: 1,
+                    dropdownColor:
+                        Theme.of(context).brightness == Brightness.dark
+                            ? Colors.black
+                            : Colors.white,
+                    focusColor: Theme.of(context).brightness == Brightness.dark
+                        ? Colors.white
+                        : Colors.black,
+                    hint: Text(
+                      'Select Day',
                     ),
-                    height: 55,
-                    child: DropdownButton<String>(
-                      focusColor:
-                          Theme.of(context).brightness == Brightness.dark
-                              ? Colors.white
-                              : Colors.black,
-                      hint: Text(
-                        'Select Day',
-                      ),
-                      isExpanded: true,
-                      value: dropDownDayValue,
-                      icon: const Icon(Icons.arrow_drop_down),
-                      iconSize: 24,
-                      underline: Container(
-                        height: 0,
-                      ),
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          dropDownDayValue = newValue!;
-                        });
-                      },
-                      items: <String>[
-                        '01',
-                        '02',
-                        '03',
-                        '04',
-                        '05',
-                        '06',
-                        '07',
-                        '08',
-                        '09',
-                        '10',
-                        '11',
-                        '12',
-                        '13',
-                        '14',
-                        '15',
-                        '16',
-                        '17',
-                        '18',
-                        '19',
-                        '20',
-                        '21',
-                        '22',
-                        '23',
-                        '24',
-                        '25',
-                        '26',
-                        '27',
-                        '28',
-                        '29',
-                        '30',
-                        '31'
-                      ].map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
+                    isExpanded: true,
+                    value: dropDownDayValue,
+                    icon: const Icon(Icons.arrow_drop_down),
+                    iconSize: 24,
+                    underline: Container(
+                      height: 0,
                     ),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        dropDownDayValue = newValue!;
+                      });
+                    },
+                    items: <String>[
+                      '01',
+                      '02',
+                      '03',
+                      '04',
+                      '05',
+                      '06',
+                      '07',
+                      '08',
+                      '09',
+                      '10',
+                      '11',
+                      '12',
+                      '13',
+                      '14',
+                      '15',
+                      '16',
+                      '17',
+                      '18',
+                      '19',
+                      '20',
+                      '21',
+                      '22',
+                      '23',
+                      '24',
+                      '25',
+                      '26',
+                      '27',
+                      '28',
+                      '29',
+                      '30',
+                      '31'
+                    ].map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        //alignment: AlignmentDirectional.bottomStart,
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
                   ),
                 ),
-                SizedBox(width: 10),
-                Expanded(
-                  child: Container(
-                    height: 52,
-                    child: DropdownButton<String>(
-                      isExpanded: true,
-                      value: dropDownMonthValue,
-                      hint: Text('Month'),
-                      icon: const Icon(Icons.arrow_drop_down),
-                      iconSize: 24,
-                      elevation: 16,
-                      underline: Container(
-                        color: Colors.grey.shade400,
-                        height: 2,
-                      ),
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          dropDownMonthValue = newValue!;
-                        });
-                      },
-                      items: <String>[
-                        '01',
-                        '02',
-                        '03',
-                        '04',
-                        '05',
-                        '06',
-                        '07',
-                        '08',
-                        '09',
-                        '10',
-                        '11',
-                        '12'
-                      ].map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
+              ),
+              SizedBox(width: 20),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).brightness == Brightness.dark
+                        ? Colors.black
+                        : Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  height: 57,
+                  child: DropdownButton<String>(
+                    enableFeedback: true,
+                    elevation: 1,
+                    dropdownColor:
+                        Theme.of(context).brightness == Brightness.dark
+                            ? Colors.black
+                            : Colors.white,
+                    focusColor: Theme.of(context).brightness == Brightness.dark
+                        ? Colors.white
+                        : Colors.black,
+                    isExpanded: true,
+                    value: dropDownMonthValue,
+                    hint: Text('Select Month'),
+                    icon: const Icon(Icons.arrow_drop_down),
+                    iconSize: 24,
+                    underline: Container(
+                      height: 0,
                     ),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        dropDownMonthValue = newValue!;
+                      });
+                    },
+                    items: <String>[
+                      '01',
+                      '02',
+                      '03',
+                      '04',
+                      '05',
+                      '06',
+                      '07',
+                      '08',
+                      '09',
+                      '10',
+                      '11',
+                      '12'
+                    ].map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
                   ),
                 ),
-              ],
-            ),
-            SizedBox(height: 20),
-            Container(
-              width: double.infinity,
-              height: 45,
-              child: ElevatedButton(
-                style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(
-                        Theme.of(context).primaryColor)),
-                onPressed: () {
-                  fetchslots();
-                },
-                child: Text('Find Slots'),
               ),
-            )
-          ]),
-        ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Container(
+            width: double.infinity,
+            height: 57,
+            child: ElevatedButton(
+              style: ButtonStyle(
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10))),
+                  backgroundColor: MaterialStateProperty.all(Colors.blue)),
+              onPressed: () {
+                fetchslots();
+              },
+              child: Text(
+                'Continue',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    color: Theme.of(context).brightness == Brightness.dark
+                        ? Colors.black
+                        : Colors.white),
+              ),
+            ),
+          ),
+        ]),
       ),
     );
   }
